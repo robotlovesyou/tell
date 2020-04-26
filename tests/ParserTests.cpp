93d34d5b7f5a35c8b06caee32eca6ed7bb2d7156
Andrew Smith <andy@robotlovesyou.com>
2020-04-26 16:36:52 +0200

    small test refactor

--- tests/ParserTests.cpp
+++ tests/ParserTests.cpp
@@ -7,12 +7,11 @@
 #include "../compiler/TypeDef.h"
 #include "ParserTestHelpers.h"
 
-
 std::shared_ptr<til::ErrorReporter> test_error_reporter() {
   return std::make_shared<til::ConsoleErrorReporter>();
 }
 
-std::unique_ptr<til::Lexer> test_lexer(const char * source, const std::shared_ptr<til::ErrorReporter>& er) {
+std::unique_ptr<til::Lexer> test_lexer(const char *source, const std::shared_ptr<til::ErrorReporter> &er) {
   auto cursor = std::make_unique<til::StringCursor>(source);
   return std::make_unique<til::Lexer>(std::move(cursor), er);
 }
@@ -29,7 +28,7 @@ TEST_CASE("Parser.parse bad top level token") {
 TEST_CASE("Parser.parse directive") {
 
   SECTION("Valid directive") {
-    const char * source = R"SOURCE(
+    const char *source = R"SOURCE(
 /// This is a comment about the directive
 /// which is continued here
 !a_directive "the directive value"
@@ -40,29 +39,29 @@ TEST_CASE("Parser.parse directive") {
 
     til::Parser p(std::move(tl), er);
     auto ast = p.Parse();
-    CHECK(ast->DeclarationCount() == 1);
-    CHECK(ast->Declaration(0)->t() == til::Declaration::kDirective);
-    auto dd = dynamic_cast<const til::DirectiveDeclaration*>(ast->Declaration(0));
-    CHECK(dd->doc().content() == "This is a comment about the directive\nwhich is continued here");
-    CHECK(dd->name() == "a_directive");
-    CHECK(dd->value() == "the directive value");
+    CHECK(ast->DeclarationCount()==1);
+    CHECK(ast->Declaration(0)->t()==til::Declaration::kDirective);
+    auto dd = dynamic_cast<const til::DirectiveDeclaration *>(ast->Declaration(0));
+    CHECK(dd->doc().content()=="This is a comment about the directive\nwhich is continued here");
+    CHECK(dd->name()=="a_directive");
+    CHECK(dd->value()=="the directive value");
     CHECK_FALSE(er->has_errors());
   }
 
   SECTION("No ident") {
-    const char *source=R"SOURCE(
+    const char *source = R"SOURCE(
 !"the directive value"
 )SOURCE";
     auto er = test_error_reporter();
     auto tl = test_lexer(source, er);
     til::Parser p(std::move(tl), er);
     auto ast = p.Parse();
-    CHECK(ast->DeclarationCount() == 0);
+    CHECK(ast->DeclarationCount()==0);
     CHECK(er->has_errors());
   }
 
   SECTION("No value") {
-    const char *source=R"SOURCE(
+    const char *source = R"SOURCE(
 !a_directive
 )SOURCE";
 
@@ -70,47 +69,54 @@ TEST_CASE("Parser.parse directive") {
     auto tl = test_lexer(source, er);
     til::Parser p(std::move(tl), er);
     auto ast = p.Parse();
-    CHECK(ast->DeclarationCount() == 0);
+    CHECK(ast->DeclarationCount()==0);
     CHECK(er->has_errors());
   }
 
   SECTION("No line feed after directive") {
-    const char*source=R"SOURCE(
+    const char *source = R"SOURCE(
 !a_directive "the directive value")SOURCE";
 
     auto er = test_error_reporter();
     auto tl = test_lexer(source, er);
     til::Parser p(std::move(tl), er);
     auto ast = p.Parse();
-    CHECK(ast->DeclarationCount() == 0);
+    CHECK(ast->DeclarationCount()==0);
     CHECK(er->has_errors());
   }
 }
 
-std::tuple<int, bool, bool, std::string, til::ScalarTypeDef::ScalarType> scalar_test_opt(int idx, bool optional, bool has_docs, std::string name, til::ScalarTypeDef::ScalarType t) {
+std::tuple<int, bool, bool, std::string, til::ScalarTypeDef::ScalarType> scalar_test_opt(int idx,
+                                                                                         bool optional,
+                                                                                         bool has_docs,
+                                                                                         std::string name,
+                                                                                         til::ScalarTypeDef::ScalarType t) {
   return std::tuple<int, bool, bool, std::string, til::ScalarTypeDef::ScalarType>(idx, optional, has_docs, name, t);
 }
 
-TEST_CASE("Parser.parse message") {
-  SECTION("Message with no fields") {
-    const char *source=R"SOURCE(
+TEST_CASE("Parser.parse message with no fields") {
+
+  const char *source = R"SOURCE(
 message an_empty_message {
 }
 )SOURCE";
 
-    auto er = test_error_reporter();
-    auto tl = test_lexer(source, er);
-    til::Parser p(std::move(tl), er);
-    auto ast = p.Parse();
-    CHECK_FALSE (er->has_errors());
-    CHECK(ast->DeclarationCount() == 1);
-    CHECK(ast->Declaration(0)->t() == til::Declaration::kMessage);
-    auto md = dynamic_cast<const til::MessageDeclaration*>(ast->Declaration(0));
-    CHECK(md->name() == "an_empty_message");
-  }
+  auto er = test_error_reporter();
+  auto tl = test_lexer(source, er);
+  til::Parser p(std::move(tl), er);
+  auto ast = p.Parse();
+  CHECK_FALSE (er->has_errors());
+  CHECK(ast->DeclarationCount()==1);
+  CHECK(ast->Declaration(0)->t()==til::Declaration::kMessage);
+  auto md = dynamic_cast<const til::MessageDeclaration *>(ast->Declaration(0));
+  CHECK(md->name()=="an_empty_message");
 
-  SECTION("Message with scalar fields") {
-    const char *source=R"SOURCE(
+}
+
+TEST_CASE("Parser.parse message with scalar fields") {
+
+  const char *source = R"SOURCE(
+/// doc comment for a_scalar_only_message
 message a_scalar_only_message {
   a_bool_field: bool
 
@@ -132,44 +138,44 @@ message a_scalar_only_message {
 }
 )SOURCE";
 
-    auto er = test_error_reporter();
-    auto tl = test_lexer(source, er);
-    til::Parser p(std::move(tl), er);
-    auto ast = p.Parse();
-    CHECK_FALSE(er->has_errors());
-    CHECK(ast->DeclarationCount() == 1);
-    CHECK(ast->Declaration(0)->t() == til::Declaration::kMessage);
-    auto md = dynamic_cast<const til::MessageDeclaration*>(ast->Declaration(0));
-    CHECK(md->name() == "a_scalar_only_message");
-    CHECK(md->FieldCount() == 10);
+  auto er = test_error_reporter();
+  auto tl = test_lexer(source, er);
+  til::Parser p(std::move(tl), er);
+  auto ast = p.Parse();
+  CHECK_FALSE(er->has_errors());
+  CHECK(ast->DeclarationCount()==1);
+  CHECK(ast->Declaration(0)->t()==til::Declaration::kMessage);
+  CHECK(ast->Declaration(0)->doc().has_content());
+  auto md = dynamic_cast<const til::MessageDeclaration *>(ast->Declaration(0));
+  CHECK(md->name()=="a_scalar_only_message");
+  CHECK(md->FieldCount()==10);
 
-    auto opts = GENERATE(
-        scalar_test_opt(0, false, false, "a_bool_field", til::ScalarTypeDef::kBool),
-        scalar_test_opt(1, true, true, "an_optional_bool_field", til::ScalarTypeDef::kBool),
-        scalar_test_opt(2, false, false, "a_float_field", til::ScalarTypeDef::kFloat),
-        scalar_test_opt(3, true, false, "an_optional_float_field", til::ScalarTypeDef::kFloat),
-        scalar_test_opt(4, false, true, "an_int_field", til::ScalarTypeDef::kInt),
-        scalar_test_opt(5, true, false, "an_optional_int_field", til::ScalarTypeDef::kInt),
-        scalar_test_opt(6, false, false, "a_string_field", til::ScalarTypeDef::kString),
-        scalar_test_opt(7, true, false, "an_optional_string_field", til::ScalarTypeDef::kString),
-        scalar_test_opt(8, false, true, "a_time_field", til::ScalarTypeDef::kTime),
-        scalar_test_opt(9, true, false, "an_optional_time_field", til::ScalarTypeDef::kTime)
-        );
+  auto opts = GENERATE(
+      scalar_test_opt(0, false, false, "a_bool_field", til::ScalarTypeDef::kBool),
+      scalar_test_opt(1, true, true, "an_optional_bool_field", til::ScalarTypeDef::kBool),
+      scalar_test_opt(2, false, false, "a_float_field", til::ScalarTypeDef::kFloat),
+      scalar_test_opt(3, true, false, "an_optional_float_field", til::ScalarTypeDef::kFloat),
+      scalar_test_opt(4, false, true, "an_int_field", til::ScalarTypeDef::kInt),
+      scalar_test_opt(5, true, false, "an_optional_int_field", til::ScalarTypeDef::kInt),
+      scalar_test_opt(6, false, false, "a_string_field", til::ScalarTypeDef::kString),
+      scalar_test_opt(7, true, false, "an_optional_string_field", til::ScalarTypeDef::kString),
+      scalar_test_opt(8, false, true, "a_time_field", til::ScalarTypeDef::kTime),
+      scalar_test_opt(9, true, false, "an_optional_time_field", til::ScalarTypeDef::kTime)
+  );
 
-    auto idx = std::get<0>(opts);
-    auto optional = std::get<1>(opts);
-    auto has_docs = std::get<2>(opts);
-    auto name = std::get<3>(opts);
-    auto scalar_type = std::get<4>(opts);
+  auto idx = std::get<0>(opts);
+  auto optional = std::get<1>(opts);
+  auto has_docs = std::get<2>(opts);
+  auto name = std::get<3>(opts);
+  auto scalar_type = std::get<4>(opts);
 
-    INFO(name);
-    CHECK(md->FieldEntry(idx).type_def()->t() == til::TypeDef::kScalar);
-    CHECK(md->FieldEntry(idx).type_def()->optional() == optional);
-    CHECK(md->FieldEntry(idx).name() == name);
-    CHECK(md->FieldEntry(idx).doc().has_content() == has_docs);
-    auto std = dynamic_cast<const til::ScalarTypeDef*>(md->FieldEntry(idx).type_def());
-    CHECK(std->scalar_type() == scalar_type);
-  }
+  INFO(name);
+  CHECK(md->FieldEntry(idx).type_def()->t()==til::TypeDef::kScalar);
+  CHECK(md->FieldEntry(idx).type_def()->optional()==optional);
+  CHECK(md->FieldEntry(idx).name()==name);
+  CHECK(md->FieldEntry(idx).doc().has_content()==has_docs);
+  auto std = dynamic_cast<const til::ScalarTypeDef *>(md->FieldEntry(idx).type_def());
+  CHECK(std->scalar_type()==scalar_type);
 }
 
 TEST_CASE("Parser.parse service") {
@@ -177,6 +183,10 @@ TEST_CASE("Parser.parse service") {
 }
 
 TEST_CASE("Parser.parse message with repeated field name") {
+  FAIL("pending");
+}
+
+TEST_CASE("Parser.parse message with unowned doc comment (not commenting a field)") {
   FAIL("pending");
 }
 
