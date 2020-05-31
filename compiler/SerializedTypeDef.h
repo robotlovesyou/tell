33b6477290157f7d7c6a960cc79681d3ce5c644b
Andrew Smith <andy@robotlovesyou.com>
2020-05-31 20:49:09 +0200

    maybe slightly tidier?

--- compiler/SerializedTypeDef.h
+++ compiler/SerializedTypeDef.h
@@ -15,11 +15,15 @@ using nlohmann::json;
 namespace til {
 struct SerializedTypeDef {
   virtual ~SerializedTypeDef() = default;
+  SerializedTypeDef() = default;
+  SerializedTypeDef(bool opt): is_optional(opt) {}
   [[nodiscard]] virtual TypeDef::Type t() const = 0;
   bool is_optional{false};
 };
 
-struct ScalarSerializedTypeDef: public SerializedTypeDef {
+struct ScalarSerializedTypeDef : public SerializedTypeDef {
+  ScalarSerializedTypeDef() = default;
+  ScalarSerializedTypeDef(ScalarTypeDef::ScalarType st, bool opt) : scalar_type(st), SerializedTypeDef(opt) {}
   [[nodiscard]] TypeDef::Type t() const override {
     return TypeDef::kScalar;
   }
@@ -39,7 +43,10 @@ void from_json(const json &j, ScalarSerializedTypeDef &sstd) {
   j.at("is_optional").get_to(sstd.is_optional);
 }
 
-struct MapSerializedTypeDef: public SerializedTypeDef {
+struct MapSerializedTypeDef : public SerializedTypeDef {
+  MapSerializedTypeDef() = default;
+  MapSerializedTypeDef(std::unique_ptr<SerializedTypeDef> sub_t, bool opt) : sub_type(std::move(sub_t)),
+                                                                                      SerializedTypeDef(opt) {}
   [[nodiscard]] TypeDef::Type t() const override {
     return TypeDef::kMap;
   }
@@ -52,23 +59,23 @@ void to_json(json &j, const MapSerializedTypeDef &mstd) {
   switch (mstd.sub_type->t()) {
 
     case TypeDef::kScalar: {
-      const auto *st = dynamic_cast<const ScalarSerializedTypeDef*>(mstd.sub_type.get());
+      const auto *st = dynamic_cast<const ScalarSerializedTypeDef *>(mstd.sub_type.get());
       j["sub_type"] = *st;
       break;
     }
     case TypeDef::kMessage:break;
     case TypeDef::kMap: {
-      const auto *mt = dynamic_cast<const MapSerializedTypeDef*>(mstd.sub_type.get());
+      const auto *mt = dynamic_cast<const MapSerializedTypeDef *>(mstd.sub_type.get());
       j["sub_type"] = *mt;
       break;
     }
     case TypeDef::kList:break;
   }
 }
+
 void to_json(json &j, const std::unique_ptr<MapSerializedTypeDef> &mstd_ptr) {
   to_json(j, *mstd_ptr);
 }
-
 
 void from_json(const json &j, MapSerializedTypeDef &mstd) {
   j.at("is_optional").get_to(mstd.is_optional);
@@ -77,15 +84,14 @@ void from_json(const json &j, MapSerializedTypeDef &mstd) {
     case TypeDef::kScalar: {
       auto sstd = j.at("sub_type").get<ScalarSerializedTypeDef>();
       auto sub_type = std::make_unique<ScalarSerializedTypeDef>();
-      *sub_type = sstd;
+      *sub_type = std::move(sstd);
       mstd.sub_type = std::move(sub_type);
       break;
     }
     case TypeDef::kMap: {
       auto map_std = j.at("sub_type").get<MapSerializedTypeDef>();
       auto sub_type = std::make_unique<MapSerializedTypeDef>();
-      sub_type->is_optional = map_std.is_optional;
-      sub_type->sub_type = std::move(map_std.sub_type);
+      *sub_type = std::move(map_std);
       mstd.sub_type = std::move(sub_type);
       break;
     }
--- tests/SerializedTypeDefTests.cpp
+++ tests/SerializedTypeDefTests.cpp
@@ -6,25 +6,23 @@
 #include <memory>
 
 TEST_CASE("Serialize type defs") {
-  auto t1 = std::make_unique<til::ScalarSerializedTypeDef>();
-  t1->scalar_type = til::ScalarTypeDef::kInt;
-  t1->is_optional = true;
-  auto t2 = std::make_unique<til::MapSerializedTypeDef>();
-  auto t3 = std::make_unique<til::ScalarSerializedTypeDef>();
-  t3->scalar_type = til::ScalarTypeDef::kFloat;
-  t3->is_optional = true;
-  t2->sub_type = std::move(t3);
-  json j_t1(t1);
-  std::cout << j_t1.dump() << std::endl;
+//  auto t1 = std::make_unique<til::ScalarSerializedTypeDef>();
+//  til::ScalarSerializedTypeDef t1{til::ScalarTypeDef::kInt, true};
+
+  auto t4_ptr = std::make_unique<til::ScalarSerializedTypeDef>(til::ScalarTypeDef::kString, false);
+  auto t3_ptr = std::make_unique<til::MapSerializedTypeDef>(std::move(t4_ptr), true);
+  til::MapSerializedTypeDef t2(std::move(t3_ptr), false);
+//  json j_t1(t1);
+//  std::cout << j_t1.dump(4) << std::endl;
   json j_t2(t2);
-  std::cout << j_t2.dump() << std::endl;
-  auto t1_2 = j_t1.get<til::ScalarSerializedTypeDef>();
-  std::cout << t1_2.t() << " " << t1_2.scalar_type << " " << t1_2.is_optional << " " << std::endl;
+  std::cout << j_t2.dump(4) << std::endl;
+//  auto t1_2 = j_t1.get<til::ScalarSerializedTypeDef>();
+//  std::cout << t1_2.t() << " " << t1_2.scalar_type << " " << t1_2.is_optional << " " << std::endl;
   try {
     auto t2_2 = j_t2.get<til::MapSerializedTypeDef>();
     std::cout << t2_2.t() << " " << t2_2.is_optional << " " << t2_2.sub_type->t() << " " << t2_2.sub_type->is_optional << std::endl;
-    auto *sstd = dynamic_cast<til::ScalarSerializedTypeDef*>(t2_2.sub_type.get());
-    std::cout << sstd->scalar_type << std::endl;
+    auto *mstd = dynamic_cast<til::MapSerializedTypeDef*>(t2_2.sub_type.get());
+    std::cout << mstd->sub_type->t() << std::endl;
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
